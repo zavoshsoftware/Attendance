74059e9e93ef2b3e93fac5457efeed3cd1865522
Saeed seyfi <mr.saeidseyfi@gmail.com>
2022-10-10 17:36:39 +0330

    debug

---
+++ Attendance.Web/ViewModels/LoginToolViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+ 
+    public class LoginToolViewModel
+    {
+        public string Tool { get; set; }
+        public string Unit { get; set; }
+        public double Amount { get; set; }
+    } 
