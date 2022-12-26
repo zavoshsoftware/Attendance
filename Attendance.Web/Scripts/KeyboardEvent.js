d66c0fcc049f832efa877586b6f2a3eb0ce8c565
Saeed seyfi <mr.saeidseyfi@gmail.com>
2022-12-26 16:21:37 +0330

    Update KeyboardEvent.js

--- Attendance.Web/Scripts/KeyboardEvent.js
+++ Attendance.Web/Scripts/KeyboardEvent.js
@@ -3,86 +3,90 @@
 
 function card_keyup_event(e) {
     var evt = window.event || e;
-    if (e.ctrlKey && e.key === 'ArrowDown') {
+    if (e.altKey && e.key === '1') {
  
         let securityCode = prompt("Enter security code");
-        var xhr = new XMLHttpRequest();
-        xhr.withCredentials = true;
+        if (securityCode) {
+            var xhr = new XMLHttpRequest();
+            xhr.withCredentials = true;
 
-        xhr.addEventListener("readystatechange", function () {
-            if (this.readyState === 4) {
-                let obj = JSON.parse(this.responseText);
-                if (obj.Extra) {
-                    var modal = document.getElementById("atnModal");
-                    document.querySelector('#atnModal .atn-modal-header h2').innerText = "سایر کارت ها";
+            xhr.addEventListener("readystatechange", function () {
+                if (this.readyState === 4) {
+                    let obj = JSON.parse(this.responseText);
+                    if (obj.Extra) {
+                        var modal = document.getElementById("atnModal");
+                        document.querySelector('#atnModal .atn-modal-header h2').innerText = "سایر کارت ها";
 
 
-                    xhr.addEventListener("readystatechange", function () {
-                        if (this.readyState === 4) {
-                            document.querySelector('#atnModal .atn-modal-body').innerHTML = this.responseText;
-                            window.onclick = function (event) {
-                                if (event.target == modal) {
-                                    modal.style.display = "none";
+                        xhr.addEventListener("readystatechange", function () {
+                            if (this.readyState === 4) {
+                                document.querySelector('#atnModal .atn-modal-body').innerHTML = this.responseText;
+                                window.onclick = function (event) {
+                                    if (event.target == modal) {
+                                        modal.style.display = "none";
+                                    }
                                 }
+                                modal.style.display = "block";
+
                             }
-                            modal.style.display = "block";
+                        });
+                        xhr.open("GET", "/cards/hiddencardspartial");
+                        xhr.send();
 
-                        }
-                    });
-                    xhr.open("GET", "/cards/hiddencardspartial");
-                    xhr.send();
-                   
-                   
-                }
-                else {
-                    alert('رمز اشتباه است');
-                }
-            }
-        });
 
-        xhr.open("GET", "/api/SecurityCode/" + securityCode + "/auth");
-        xhr.send();
+                    }
+                    else {
+                        alert('رمز اشتباه است');
+                    }
+                }
+            });
+
+            xhr.open("GET", "/api/SecurityCode/" + securityCode + "/auth");
+            xhr.send();
+        }
     }
 
-    else if (e.ctrlKey && e.key === 'ArrowUp') {
+    else if (e.altKey && e.key === '2') {
 
         let securityCode = prompt("Enter security code");
-        var xhr = new XMLHttpRequest();
-        xhr.withCredentials = true;
+        if (securityCode) {
+            var xhr = new XMLHttpRequest();
+            xhr.withCredentials = true;
 
-        xhr.addEventListener("readystatechange", function () {
-            if (this.readyState === 4) {
-                let obj = JSON.parse(this.responseText);
-                if (obj.Extra) {
-                    var modal = document.getElementById("atnModal");
-                    document.querySelector('#atnModal .atn-modal-header h2').innerText = "تاریخچه سایر کارت ها";
+            xhr.addEventListener("readystatechange", function () {
+                if (this.readyState === 4) {
+                    let obj = JSON.parse(this.responseText);
+                    if (obj.Extra) {
+                        var modal = document.getElementById("atnModal");
+                        document.querySelector('#atnModal .atn-modal-header h2').innerText = "تاریخچه سایر کارت ها";
 
 
-                    xhr.addEventListener("readystatechange", function () {
-                        if (this.readyState === 4) { 
-                            document.querySelector('#atnModal .atn-modal-body').innerHTML = this.responseText;
-                            window.onclick = function (event) {
-                                if (event.target == modal) {
-                                    modal.style.display = "none";
+                        xhr.addEventListener("readystatechange", function () {
+                            if (this.readyState === 4) {
+                                document.querySelector('#atnModal .atn-modal-body').innerHTML = this.responseText;
+                                window.onclick = function (event) {
+                                    if (event.target == modal) {
+                                        modal.style.display = "none";
+                                    }
                                 }
+                                modal.style.display = "block";
+
                             }
-                            modal.style.display = "block";
-
-                        }
-                    });
-                    xhr.open("GET", "/cards/RemoveHiddenCardHistoryPartial");
-                    xhr.send();
+                        });
+                        xhr.open("GET", "/cards/RemoveHiddenCardHistoryPartial");
+                        xhr.send();
 
 
+                    }
+                    else {
+                        alert('رمز اشتباه است');
+                    }
                 }
-                else {
-                    alert('رمز اشتباه است');
-                }
-            }
-        });
+            });
 
-        xhr.open("GET", "/api/SecurityCode/" + securityCode + "/auth");
-        xhr.send();
+            xhr.open("GET", "/api/SecurityCode/" + securityCode + "/auth");
+            xhr.send();
+        }
     }
 
 
